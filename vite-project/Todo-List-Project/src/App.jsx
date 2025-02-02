a132dd0e2d46c5fd2741635916cc7036f766ffd3
sartaj <hajamsaj@gmail.com>
2025-02-02 13:17:19 +0530

    Bootstap Project Todo App

---
+++ vite-project/Todo-List-Project/src/App.jsx
@@ -0,0 +1,18 @@
+import Headname from "./components/headname";
+import Input from "./components/input";
+import Task1 from "./components/Task1";
+
+import "./App.css"
+
+
+function App() {
+  return (
+    <center>
+      <Headname />
+      <Input />
+      <Task1 />
+
+    </center>
+  );
+}
+export default App;
